4d1cd7ee9244f8beaac0db88c1c95f78c22130b5
riccardo tinto <tinto.riccardo@iisgalvanimi.edu.it>
2024-11-12 09:45:21 +0000

    commit 4 passaggio

--- commit4.py
+++ commit4.py
@@ -1,9 +1,24 @@
 import mysql.connector
 
+
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password=""
+  password="",
+  database="DatabaseVerifica"
 )
 
-mycursor = mydb.cursor()
+errore = True
+while errore:
+    mycursor = mydb.cursor()
+    mycursor.execute("USE DatabaseVerifica")
+    
+
+    selezionaId = input("Inserisci l'ID del videogioco che vuoi eliminare: ")
+    
+
+    sql = "SELECT EXISTS(SELECT 1 FROM Videogiochi WHERE id = %s)"
+    mycursor.execute(sql, (selezionaId,))
+    exists = mycursor.fetchone()[0]
+    
+    
