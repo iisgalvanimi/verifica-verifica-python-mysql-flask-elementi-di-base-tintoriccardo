5f38a842b2b36dce3353428465ef2fb204aa7a8d
riccardo tinto <tinto.riccardo@iisgalvanimi.edu.it>
2024-11-12 10:38:52 +0000

    commit punto 8

--- commit8.py
+++ commit8.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+from flask import Flask, jsonify
+
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -8,3 +10,8 @@ database="DatabaseVerifica"
 )
 
 mycursor = mydb.cursor()
+app = Flask(__name__)
+
+@app.route('/dati', methods=['GET'])
+def get_dati():
+    return jsonify(dati)
