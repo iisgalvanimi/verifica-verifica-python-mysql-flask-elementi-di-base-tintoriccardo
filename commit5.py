b2e49230bb570830d7e7d132fe62356fb01273f7
riccardo tinto <tinto.riccardo@iisgalvanimi.edu.it>
2024-11-12 09:54:28 +0000

    commit punto 5

--- commit5.py
+++ commit5.py
@@ -3,7 +3,26 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password=""
+  password="",
+database="DatabaseVerifica",
 )
 
 mycursor = mydb.cursor()
+
+
+try:
+
+    selectAnno = int(input("Qual è l'anno di uscita dei videogiochi che vuoi vedere?: "))
+    
+
+    mycursor.execute("USE DatabaseVerifica")
+    sql = "SELECT * FROM Videogiochi WHERE Anno = %s"
+    mycursor.execute(sql, (selectAnno,))
+    myresult = mycursor.fetchall()
+
+
+
+        
+except ValueError:
+   
+    print("Errore: Valore non valido. Riprova con un anno valido.")
