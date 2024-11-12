fcee9923812ffbac20ffec408fc48d8b44c94aef
riccardo tinto <tinto.riccardo@iisgalvanimi.edu.it>
2024-11-12 09:40:11 +0000

    commit 3 passaggio

--- commit3.py
+++ commit3.py
@@ -1,9 +1,28 @@
 import mysql.connector
 
+# Connessione al database
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password=""
+  password="",
+  database="DatabaseVerifica"
 )
 
 mycursor = mydb.cursor()
+
+print("INSERISCI UN NUOVO VIDEOGIOCO")
+
+Nome = input("Inserisci il nome del videogioco: ")
+Anno = int(input("Inserisci l'anno di uscita: "))
+Genere = input("Inserisci il genere del videogioco: ")
+Piattaforma = input("Inserisci la piattaforma (ad esempio, PC, PS4, Xbox): ")
+Sviluppatore = input("Inserisci il nome dello sviluppatore: ")
+
+sql = "INSERT INTO Videogiochi (Nome, Anno, Genere, Piattaforma, Sviluppatore) VALUES (%s, %s, %s, %s, %s)"
+val = (Nome, Anno, Genere, Piattaforma, Sviluppatore)
+
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+print(mycursor.rowcount, "record inserito.")
