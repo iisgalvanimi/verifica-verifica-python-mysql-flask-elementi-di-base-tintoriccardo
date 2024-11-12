fe70f1b4352be9c6ce2c954020b8c586689bf5f0
riccardo tinto <tinto.riccardo@iisgalvanimi.edu.it>
2024-11-12 09:36:54 +0000

    commit secondo passaggio

--- commit2.py
+++ commit2.py
@@ -3,7 +3,14 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password=""
+  password="",
+  database="DatabaseVerifica"
 )
 
 mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM Videogiochi")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
